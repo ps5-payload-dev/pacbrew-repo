8a747ea35468169a0682515f568913139eb6ed4b
John Törnblom <john.tornblom@gmail.com>
2024-05-29 20:01:16 +0200

    add libmagic

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PKGS=(sdk libcxx bzip2 zlib mbedtls xz libarchive lua tinyxml2 libsamplerate
-      SDL2 libpng libjpeg-turbo libwebp freetype libconfig libsodium
+      SDL2 libpng libjpeg-turbo libwebp freetype libconfig libsodium libmagic
       libmicrohttpd SDL2_mixer SDL2_ttf SDL2_image SDL2_net curl ffmpeg)
 
 sudo pacman --noconfirm --remove ps5-payload-dev
