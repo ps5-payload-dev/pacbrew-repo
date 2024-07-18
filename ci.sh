8a67875ca978bfb6348ef1cff4705e20e7f0f76f
John Törnblom <john.tornblom@gmail.com>
2024-07-18 10:18:24 +0200

    ci: only build libs to save some compute time

--- ci.sh
+++ ci.sh
@@ -11,8 +11,7 @@ PKGS=(sdk libcxx
       freetype
       libsamplerate libsodium libogg flac
       SDL2 SDL2_mixer SDL2_ttf SDL2_image SDL2_net
-      lua curl ffmpeg
-      lakesnes mednafen devilutionx)
+      lua curl ffmpeg)
 
 sudo pacman --noconfirm --remove ps5-payload-dev
 
