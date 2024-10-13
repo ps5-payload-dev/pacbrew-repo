efc2e3e8c6119d7d8915ceb5413cbeed466fc3c2
John Törnblom <john.tornblom@gmail.com>
2024-10-13 19:31:50 +0200

    opus: initial commit

--- ci.sh
+++ ci.sh
@@ -11,7 +11,7 @@ PKGS=(sdk libcxx
       sqlite libmicrohttpd websrv
       libpng libjpeg-turbo libwebp
       freetype harfbuzz fontconfig
-      libsamplerate libsodium libogg libvorbis flac
+      libsamplerate libsodium libogg libvorbis flac opus
       libass
       libvpx
       enet
